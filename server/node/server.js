6a2efdedd29ccef57740f73be2f7db9b68b9e657
adwaitkaley <adwait.kaley@gmail.com>
2015-06-26 16:37:50 -0700

    Modified Modular js errors

--- server/node/server.js
+++ server/node/server.js
@@ -21,6 +21,14 @@ var request = require('request');
 
 var config = require('./config');
 
+var fb = require('./services/api/connectFacebook');
+var gmail = require('./services/api/connectGoogle');
+var twitter = require('./services/api/connectTwitter');
+
+var User = require('./services/database/mongo');
+var userSchema = require('./services/database/mongo');
+
+/*
 var userSchema = new mongoose.Schema({
   email: { type: String, unique: true, lowercase: true },
   password: { type: String, select: false },
@@ -61,6 +69,8 @@ mongoose.connect(config.MONGO_URI);
 mongoose.connection.on('error', function(err) {
   console.log('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
 });
+/*
+ */
 
 var app = express();
 
@@ -118,7 +128,7 @@ function createJWT(user) {
   };
   return jwt.encode(payload, config.TOKEN_SECRET);
 }
-
+exports.createJWT=createJWT;
 /*
  |--------------------------------------------------------------------------
  | GET /api/me
@@ -175,7 +185,8 @@ app.post('/auth/login', function(req, res) {
  */
 app.post('/auth/signup', function(req, res) {
 	console.log('test1');
-  User.findOne({ email: req.body.email }, function(err, existingUser) {
+  User.User.findOne({ email: req.body.email }, function(err, existingUser) {
+	  console.log('findOne');
     if (existingUser) {
       return res.status(409).send({ message: 'Email is already taken' });
     }
@@ -185,6 +196,7 @@ app.post('/auth/signup', function(req, res) {
       password: req.body.password
     });
     user.save(function() {
+    	console.log('save');
       res.send({ token: createJWT(user) });
     });
   });
@@ -195,72 +207,15 @@ app.post('/auth/signup', function(req, res) {
  | Login with Google
  |--------------------------------------------------------------------------
  */
-app.post('/auth/google', function(req, res) {
-  var accessTokenUrl = 'https://accounts.google.com/o/oauth2/token';
-  var peopleApiUrl = 'https://www.googleapis.com/plus/v1/people/me/openIdConnect';
-  var params = {
-    code: req.body.code,
-    client_id: req.body.clientId,
-    client_secret: config.GOOGLE_SECRET,
-    redirect_uri: req.body.redirectUri,
-    grant_type: 'authorization_code'
-  };
+app.post('/auth/google', gmail.connectGmail);
 
-  // Step 1. Exchange authorization code for access token.
-  request.post(accessTokenUrl, { json: true, form: params }, function(err, response, token) {
-    var accessToken = token.access_token;
-    var headers = { Authorization: 'Bearer ' + accessToken };
-
-    // Step 2. Retrieve profile information about the current user.
-    request.get({ url: peopleApiUrl, headers: headers, json: true }, function(err, response, profile) {
-
-      // Step 3a. Link user accounts.
-      if (req.headers.authorization) {
-        User.findOne({ google: profile.sub }, function(err, existingUser) {
-          if (existingUser) {
-            return res.status(409).send({ message: 'There is already a Google account that belongs to you' });
-          }
-          var token = req.headers.authorization.split(' ')[1];
-          var payload = jwt.decode(token, config.TOKEN_SECRET);
-          User.findById(payload.sub, function(err, user) {
-            if (!user) {
-              return res.status(400).send({ message: 'User not found' });
-            }
-            user.google = profile.sub;
-            user.picture = user.picture || profile.picture.replace('sz=50', 'sz=200');
-            user.displayName = user.displayName || profile.name;
-            user.save(function() {
-              var token = createJWT(user);
-              res.send({ token: token });
-            });
-          });
-        });
-      } else {
-        // Step 3b. Create a new user account or return an existing one.
-        User.findOne({ google: profile.sub }, function(err, existingUser) {
-          if (existingUser) {
-            return res.send({ token: createJWT(existingUser) });
-          }
-          var user = new User();
-          user.google = profile.sub;
-          user.picture = profile.picture.replace('sz=50', 'sz=200');
-          user.displayName = profile.name;
-          user.save(function(err) {
-            var token = createJWT(user);
-            res.send({ token: token });
-          });
-        });
-      }
-    });
-  });
-});
 
 /*
  |--------------------------------------------------------------------------
  | Login with GitHub
  |--------------------------------------------------------------------------
  */
-app.post('/auth/github', function(req, res) {
+/*app.post('/auth/github', function(req, res) {
   var accessTokenUrl = 'https://github.com/login/oauth/access_token';
   var userApiUrl = 'https://api.github.com/user';
   var params = {
@@ -319,13 +274,14 @@ app.post('/auth/github', function(req, res) {
     });
   });
 });
+*/
 
 /*
  |--------------------------------------------------------------------------
  | Login with LinkedIn
  |--------------------------------------------------------------------------
  */
-app.post('/auth/linkedin', function(req, res) {
+/*app.post('/auth/linkedin', function(req, res) {
   var accessTokenUrl = 'https://www.linkedin.com/uas/oauth2/accessToken';
   var peopleApiUrl = 'https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address,picture-url)';
   var params = {
@@ -389,13 +345,14 @@ app.post('/auth/linkedin', function(req, res) {
     });
   });
 });
+*/
 
 /*
  |--------------------------------------------------------------------------
  | Login with Windows Live
  |--------------------------------------------------------------------------
  */
-app.post('/auth/live', function(req, res) {
+/*app.post('/auth/live', function(req, res) {
   async.waterfall([
     // Step 1. Exchange authorization code for access token.
     function(done) {
@@ -457,80 +414,22 @@ app.post('/auth/live', function(req, res) {
     }
   ]);
 });
+*/
 
 /*
  |--------------------------------------------------------------------------
  | Login with Facebook
  |--------------------------------------------------------------------------
  */
-app.post('/auth/facebook', function(req, res) {
-  var accessTokenUrl = 'https://graph.facebook.com/v2.3/oauth/access_token';
-  var graphApiUrl = 'https://graph.facebook.com/v2.3/me';
-  var params = {
-    code: req.body.code,
-    client_id: req.body.clientId,
-    client_secret: config.FACEBOOK_SECRET,
-    redirect_uri: req.body.redirectUri
-  };
+app.post('/auth/facebook', fb.connectFB);
 
-  // Step 1. Exchange authorization code for access token.
-  request.get({ url: accessTokenUrl, qs: params, json: true }, function(err, response, accessToken) {
-    if (response.statusCode !== 200) {
-      return res.status(500).send({ message: accessToken.error.message });
-    }
-
-    // Step 2. Retrieve profile information about the current user.
-    request.get({ url: graphApiUrl, qs: accessToken, json: true }, function(err, response, profile) {
-      if (response.statusCode !== 200) {
-        return res.status(500).send({ message: profile.error.message });
-      }
-      if (req.headers.authorization) {
-        User.findOne({ facebook: profile.id }, function(err, existingUser) {
-          if (existingUser) {
-            return res.status(409).send({ message: 'There is already a Facebook account that belongs to you' });
-          }
-          var token = req.headers.authorization.split(' ')[1];
-          var payload = jwt.decode(token, config.TOKEN_SECRET);
-          User.findById(payload.sub, function(err, user) {
-            if (!user) {
-              return res.status(400).send({ message: 'User not found' });
-            }
-            user.facebook = profile.id;
-            user.picture = user.picture || 'https://graph.facebook.com/v2.3/' + profile.id + '/picture?type=large';
-            user.displayName = user.displayName || profile.name;
-            user.save(function() {
-              var token = createJWT(user);
-              res.send({ token: token });
-            });
-          });
-        });
-      } else {
-        // Step 3b. Create a new user account or return an existing one.
-        User.findOne({ facebook: profile.id }, function(err, existingUser) {
-          if (existingUser) {
-            var token = createJWT(existingUser);
-            return res.send({ token: token });
-          }
-          var user = new User();
-          user.facebook = profile.id;
-          user.picture = 'https://graph.facebook.com/' + profile.id + '/picture?type=large';
-          user.displayName = profile.name;
-          user.save(function() {
-            var token = createJWT(user);
-            res.send({ token: token });
-          });
-        });
-      }
-    });
-  });
-});
 
 /*
  |--------------------------------------------------------------------------
  | Login with Yahoo
  |--------------------------------------------------------------------------
  */
-app.post('/auth/yahoo', function(req, res) {
+/*app.post('/auth/yahoo', function(req, res) {
   var accessTokenUrl = 'https://api.login.yahoo.com/oauth2/get_token';
   var clientId = req.body.clientId;
   var clientSecret = config.YAHOO_SECRET;
@@ -587,109 +486,21 @@ app.post('/auth/yahoo', function(req, res) {
     });
   });
 });
+*/
 
 /*
  |--------------------------------------------------------------------------
  | Login with Twitter
  |--------------------------------------------------------------------------
  */
-app.post('/auth/twitter', function(req, res) {
-  var requestTokenUrl = 'https://api.twitter.com/oauth/request_token';
-  var accessTokenUrl = 'https://api.twitter.com/oauth/access_token';
-  var profileUrl = 'https://api.twitter.com/1.1/users/show.json?screen_name=';
-
-  // Part 1 of 2: Initial request from Satellizer.
-  if (!req.body.oauth_token || !req.body.oauth_verifier) {
-    var requestTokenOauth = {
-      consumer_key: config.TWITTER_KEY,
-      consumer_secret: config.TWITTER_SECRET,
-      callback: config.TWITTER_CALLBACK
-    };
-
-    // Step 1. Obtain request token for the authorization popup.
-    request.post({ url: requestTokenUrl, oauth: requestTokenOauth }, function(err, response, body) {
-      var oauthToken = qs.parse(body);
-
-      // Step 2. Send OAuth token back to open the authorization screen.
-      res.send(oauthToken);
-    });
-  } else {
-    // Part 2 of 2: Second request after Authorize app is clicked.
-    var accessTokenOauth = {
-      consumer_key: config.TWITTER_KEY,
-      consumer_secret: config.TWITTER_SECRET,
-      token: req.body.oauth_token,
-      verifier: req.body.oauth_verifier
-    };
-
-    // Step 3. Exchange oauth token and oauth verifier for access token.
-    request.post({ url: accessTokenUrl, oauth: accessTokenOauth }, function(err, response, accessToken) {
-
-      accessToken = qs.parse(accessToken);
-
-      var profileOauth = {
-        consumer_key: config.TWITTER_KEY,
-        consumer_secret: config.TWITTER_SECRET,
-        oauth_token: accessToken.oauth_token
-      };
-
-      // Step 4. Retrieve profile information about the current user.
-      request.get({
-        url: profileUrl + accessToken.screen_name,
-        oauth: profileOauth,
-        json: true
-      }, function(err, response, profile) {
-
-        // Step 5a. Link user accounts.
-        if (req.headers.authorization) {
-          User.findOne({ twitter: profile.id }, function(err, existingUser) {
-            if (existingUser) {
-              return res.status(409).send({ message: 'There is already a Twitter account that belongs to you' });
-            }
-
-            var token = req.headers.authorization.split(' ')[1];
-            var payload = jwt.decode(token, config.TOKEN_SECRET);
-
-            User.findById(payload.sub, function(err, user) {
-              if (!user) {
-                return res.status(400).send({ message: 'User not found' });
-              }
-
-              user.twitter = profile.id;
-              user.displayName = user.displayName || profile.name;
-              user.picture = user.picture || profile.profile_image_url.replace('_normal', '');
-              user.save(function(err) {
-                res.send({ token: createJWT(user) });
-              });
-            });
-          });
-        } else {
-          // Step 5b. Create a new user account or return an existing one.
-          User.findOne({ twitter: profile.id }, function(err, existingUser) {
-            if (existingUser) {
-              return res.send({ token: createJWT(existingUser) });
-            }
-
-            var user = new User();
-            user.twitter = profile.id;
-            user.displayName = profile.name;
-            user.picture = profile.profile_image_url.replace('_normal', '');
-            user.save(function() {
-              res.send({ token: createJWT(user) });
-            });
-          });
-        }
-      });
-    });
-  }
-});
+app.post('/auth/twitter', twitter.connectTwitter);
 
 /*
  |--------------------------------------------------------------------------
  | Login with Foursquare
  |--------------------------------------------------------------------------
  */
-app.post('/auth/foursquare', function(req, res) {
+/*app.post('/auth/foursquare', function(req, res) {
   var accessTokenUrl = 'https://foursquare.com/oauth2/access_token';
   var profileUrl = 'https://api.foursquare.com/v2/users/self';
   var formData = {
@@ -752,7 +563,7 @@ app.post('/auth/foursquare', function(req, res) {
     });
   });
 });
-
+*/
 
 /*
  |--------------------------------------------------------------------------
---
+++ server/node/services/database/mongo.js
@@ -0,0 +1,63 @@
+var path = require('path');
+var qs = require('querystring');
+
+var async = require('async');
+var bcrypt = require('bcryptjs');
+var bodyParser = require('body-parser');
+var colors = require('colors');
+var cors = require('cors');
+var express = require('express');
+var logger = require('morgan');
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var mongoose = require('mongoose');
+var request = require('request');
+
+var config = require('../../config');
+
+var userSchema = new mongoose.Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String, select: false },
+  displayName: String,
+  picture: String,
+  facebook: String,
+  foursquare: String,
+  google: String,
+  github: String,
+  linkedin: String,
+  live: String,
+  yahoo: String,
+  twitter: String
+});
+exports.userSchema=userSchema;
+
+exports.userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+exports.userSchema.methods.comparePassword = function(password, done) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    done(err, isMatch);
+  });
+};
+
+var User = mongoose.model('User', userSchema);
+
+exports.User=User;
+
+mongoose.connect(config.MONGO_URI);
+mongoose.connection.on('error', function(err) {
+  console.log('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
+});
+
+//exports.userSchema=userSchema;
+//exports.User=User;
