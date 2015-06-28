8762d5617db1daf80355db23904fbfd9fa740474
aksharjoshi <Akshar@Akshars-MacBook-Pro.local>
2015-06-27 23:32:01 -0700

    sql integration started

--- server/node/services/api/connectGoogle.js
+++ server/node/services/api/connectGoogle.js
@@ -12,7 +12,10 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 var mongoose = require('mongoose');
 var request = require('request');
-var jwtServer=require('../../server')
+
+var mysql = require('../database/mysql');
+
+var jwtServer=require('../../server');
 var config = require('../../config');
 
 var User = require('../database/mongo');
@@ -36,51 +39,108 @@ var accessTokenUrl = 'https://accounts.google.com/o/oauth2/token';
     var headers = { Authorization: 'Bearer ' + accessToken };
     var headers = { Authorization: 'Bearer ' + accessToken };
 
-    // Step 2. Retrieve profile information about the current user.
+ // Step 2. Retrieve profile information about the current user.
     request.get({ url: peopleApiUrl, headers: headers, json: true }, function(err, response, profile) {
 
-	
-	
       // Step 3a. Link user accounts.
       if (req.headers.authorization) {
-        User.User.findOne({ google: profile.sub }, function(err, existingUser) {
-          if (existingUser) {
-            return res.status(409).send({ message: 'There is already a Google account that belongs to you' });
-          }
-          var token = req.headers.authorization.split(' ')[1];
-          var payload = jwt.decode(token, config.TOKEN_SECRET);
-          User.User.findById(payload.sub, function(err, user) {
-            if (!user) {
-              return res.status(400).send({ message: 'User not found' });
-            }
-			console.log(profile.sub);
-            user.google = profile.sub;
-            user.picture = user.picture || profile.picture.replace('sz=50', 'sz=200');
-            user.displayName = user.displayName || profile.name;
-            user.save(function() {
-              var token = jwtServer.createJWT(user);
-              res.send({ token: token });
-            });
-          });
-        });
-      } else {
-        // Step 3b. Create a new user account or return an existing one.
-        User.User.findOne({ google: profile.sub }, function(err, existingUser) {
-          if (existingUser) {
-            return res.send({ token: jwtServer.createJWT(existingUser) });
-          }
-          var user = new User.User();
-		  console.log(profile.sub);
-          user.google = profile.sub;
-          user.picture = profile.picture.replace('sz=50', 'sz=200');
-          user.displayName = profile.name;
-          user.save(function(err) {
-            var token = jwtServer.createJWT(user);
-            res.send({ token: token });
-          });
-        });
-      }
-    });
+    	 
+    	  var query="select U_ID from db_pixel.USR_DTL_TBL where U_ID='"+profile.sub+"'";
+    	 
+    	  mysql.dbcall(function(err,results){
+    		  if(err){
+    			  throw err;
+    		  }
+    		  else 
+    		  {
+    			  if(results) {
+        			  console.log("user looking up");
+        			  return res.status(409).send({ message: 'There is already a Google account that belongs to you' });
+    			  }
+    			  //res.send({"save":"Success"});
+    		  }  
+    	  },query);
+    	 
+    	  var token = req.headers.authorization.split(' ')[1];
+    	  var payload = jwt.decode(token, config.TOKEN_SECRET);
+          
+    	  var query="select U_ID from db_pixel.USR_DTL_TBL where U_ID='"+payload.sub+"'";
+ 	 
+    	  mysql.dbcall(function(err,results){
+    		  if(err){
+    			  throw err;
+    		  }
+    		  else 
+    		  {
+    			  if(!results) {
+    				  console.log("user looking up");
+    				  return res.status(400).send({ message: 'User not found' });    		  
+    			  }
+    		  }  
+    	  },query);
+      
+    	  console.log(profile.sub);
+      
+    	  var sqlQuery="insert into db_pixel.USR_DTL_TBL (U_ID,UNAME,PICTURE,SRC,CREATION_DATE)" +
+      		"VALUES ('"+profile.sub+"','"+profile.name+"','"+profile.picture.replace('sz=50', 'sz=200')+
+      		"','G',SYSDATE()";  
+
+    	  mysql.dbcall(function(err,results){
+    		  if(err){
+    			  throw err;
+    		  }
+    		  else 
+    		  {
+    			  console.log("user inserted");
+    			  var token = jwtServer.createJWT(user);
+    			  res.send({ token: token });
+    		  }  
+    	  },sqlQuery);
+      
+      	} 
+      	else {
+      		// Step 3b. Create a new user account or return an existing one.
+      		
+      		var query="select U_ID from db_pixel.USR_DTL_TBL where U_ID='"+profile.sub+"'";
+       	 
+      		mysql.dbcall(function(err,results){
+      			if(err){
+      				throw err;
+      			}
+      			else 
+      			{
+      				if(results){
+          				console.log("user looking up");
+          				return res.send({ token: jwtServer.createJWT(existingUser) });
+      				}
+      				
+      			  //res.send({"save":"Success"});
+      			}  
+      		},query);
+      		
+      		User.User.findOne({ google: profile.sub }, function(err, existingUser) {
+      			if (existingUser) {
+      				return res.send({ token: jwtServer.createJWT(existingUser) });
+      			}
+      			
+      			var sqlQuery="insert into db_pixel.USR_DTL_TBL (U_ID,UNAME,PICTURE,SRC,CREATION_DATE)" +
+          		"VALUES ('"+profile.sub+"','"+profile.name+"','"+profile.picture.replace('sz=50', 'sz=200')+
+          		"','G',SYSDATE()";  
+
+        	  mysql.dbcall(function(err,results){
+        		  if(err){
+        			  throw err;
+        		  }
+        		  else 
+        		  {
+        			  console.log("user inserted");
+        			  var token = jwtServer.createJWT(user);
+        			  res.send({ token: token });
+        		  }  
+        	  },sqlQuery);
+      		});
+      	}
+    	});
   });
 };
 
